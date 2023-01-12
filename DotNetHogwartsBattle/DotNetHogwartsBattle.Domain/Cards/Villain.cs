1d214b8c9948615412c44d6d7c21ca1857d24011
Nick Thys <nick.thys@involved-it.be>
2023-01-12 22:08:40 +0100

    Created villain card

---
+++ DotNetHogwartsBattle/DotNetHogwartsBattle.Domain/Cards/Villain.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetHogwartsBattle.Domain.Cards;
+
+public class Villain : Card
+{
+    public Ability Ability { get; set; }
+    public int Health { get; set; }
+    public Reward Reward { get; set; }
+
+    public override string ToString()
+    {
+        return "Ability: " + Ability + "\nHealth: " + Health + "\n Reward: " + Reward;
+    }
+}
--- DotNetHogwartsBattle/DotNetHogwartsBattle.Domain/Reward.cs
+++ DotNetHogwartsBattle/DotNetHogwartsBattle.Domain/Reward.cs
@@ -2,5 +2,10 @@
 public class Reward
 {
     public int Id { get; set; }
-    public IEnumerable<Action> Actions{ get; set; }
+    public IEnumerable<Action> Actions { get; set; }
+
+    public override string ToString()
+    {
+        return string.Join("\n", Actions);
+    }
 }
